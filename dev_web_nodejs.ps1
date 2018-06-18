fb1a837c889520a8328a6751333790a039019902
molant <amolleda@gmail.com>
2018-06-18 12:46:41 -0700

    New: Install node via `nvs` and use `windows-build-tools`
    * Use `nvs` to install LTS and latest version of node.
    * Install `windows-build-tools` instead of independent packages for
      building node binary packages.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: #46

--- dev_web_nodejs.ps1
+++ dev_web_nodejs.ps1
@@ -38,13 +38,16 @@ choco install -y inconsolata
 # choco install -y ubuntu.font
 
 #--- Tools ---
-choco install -y nodejs-lts # Node.js LTS, Recommended for most users
-# choco install -y nodejs # Node.js Current, Latest features
-choco install -y visualstudio2017buildtools
-choco install -y visualstudio2017-workload-vctools
+choco install -y nvs
+refreshenv
+nvs install latest
+nvs install lst
+nvs link lts
+npm install -g windows-build-tools
+
 choco install -y sysinternals
 choco install -y docker-for-windows
-choco install -y python2 # Node.js requires Python 2 to build native modules
+# choco install -y python2 # Node.js requires Python 2 to build native modules
 
 Enable-UAC
 Enable-MicrosoftUpdate
